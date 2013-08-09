edc08cc3d26c686753ff710b4ebbb524b57d4066
Padma <ppriya.v@gmail.com>
2013-08-08 18:01:56 -0700

    Fixed github issue

--- js/application.js
+++ js/application.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
-	$(".tasks").hover(function () {
-		$("#main-slogan").css("font-size","45px");
+	
+	$("#basecamp-mod").hide();
+	$("#highrise-mod").hide();
+	$("#campfire-mod").hide();
+	
+	$(".tasks").hover(function (event) {
+		$("h1").css("font-size","45px");
 		$("h3").css("font-size","15px");
-		$("#main-slogan").text("Basecamp is the project management tool you wish you had on your last project."),
-		$("h3").text("Are you still managing projects with email? Are you still using Excel for your to-do lists? It's time to upgrade to Basecamp. Manage projects and collaborate with your clients the modern way.");
+		
+		$("#company-heading").hide();				
+		$("#"+event.target.id + "-mod").show();
+		
 	  },
 	  function () {
-	    $("#main-slogan").css("font-size","50px");
-		$("h3").css("font-size","20px");
-		$("#main-slogan").text("Making collaboration productive and enjoyable for people every day.");
-		$("h3").text("Frustration-free web-based apps for collaboration, sharing information, and making decisions.")
-		}
+	   	$("#basecamp-mod").hide();
+		$("#highrise-mod").hide();
+		$("#campfire-mod").hide();
+		$("#company-heading").show();	
+	  }
 	);
 	
 });
