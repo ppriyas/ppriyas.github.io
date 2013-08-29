2e13e48ac8462e1e1bfd58ac70ef28572d600ab8
Padma <ppriya.v@gmail.com>
2013-08-28 19:47:28 -0700

    hiding the secondary header in the css now, so that it does not get displayed intermittently when the page is initially loading

--- js/application.js
+++ js/application.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
 	
-	$(".secondary-header").hide();	
-	
+		
 	$(".tasks").hover(function (event) {
-		//$("h1").css("font-size","45px");
-		//$("h3").css("font-size","15px");
 		
 		$("#company-heading").hide();	
 		var elem_id = $(this).attr("id");
@@ -20,3 +17,4 @@ $(document).ready(function() {
 	);
 	
 });
+
